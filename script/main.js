192db30cee8f6a09deceaf6ce7578ac21dfd6485
Indra Soraya <indrasoraya96@gmail.com>
2023-01-29 22:22:19 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
   Swal.fire({
-    title: 'Apakah kamu ingin melihatnya sembari mendengarkan lagu?',
+    title: 'Mau sambil denger lagu gak ?',
     // text: "You won't be able to revert this!",
     icon: 'warning',
     showCancelButton: true,
